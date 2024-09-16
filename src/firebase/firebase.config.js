d10adcd350188c20295139b9e4dacaa139ac650a
contactmizan <contactmizan1206@gmail.com>
2024-09-16 16:00:17 +0600

    set up router-provider & create root

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBXMjs7pu_QnL8dckyCqHtozc7XvUTRcg8",
+    authDomain: "real-state-93e28.firebaseapp.com",
+    projectId: "real-state-93e28",
+    storageBucket: "real-state-93e28.appspot.com",
+    messagingSenderId: "93174569368",
+    appId: "1:93174569368:web:53f09c3a0402b90ba933ed"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
